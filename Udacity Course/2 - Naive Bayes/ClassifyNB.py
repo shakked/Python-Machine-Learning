217d7924ce65dfae323025a78f55a674e640fd8a
Zachary Shakked <zshakked@stevens.edu>
2016-05-03 11:04:04 -0400

    Naive Bayes Lecture

---
+++ Udacity Course/2 - Naive Bayes/ClassifyNB.py
@@ -0,0 +1,13 @@
+from sklearn.naive_bayes import GaussianNB
+
+def classify(features_train, labels_train):   
+	clf = GaussianNB()
+	clf.fit(features_train, labels_train)
+	return clf
+    ### create classifier
+    ### fit the classifier on the training features and labels
+    ### return the fit classifier
+    
+        
+    ### your code goes here!
+    
